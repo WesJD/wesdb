fb1c124572e390ddd4996af2b478f88682458494
Wesley Smith <me@wesjd.net>
2022-12-30 11:15:42 -0500

    add stupid write

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,18 @@ const start = async (port: number) => {
             return
         }
 
-        db.all(request.body.sql)
+        const sql = request.body.sql as string
+        // This is a REALLY stupid check to make sure no one does an update inside of a
+        // query. Of course, this would never be a real solution in production, but no need
+        // to overcomplicate our simple test project.
+        const lowerSql = sql.toLowerCase()
+        if (["update", "insert", "alter", "create"].some((elem) => lowerSql.includes(elem))) {
+            response.status(400)
+            response.json({ error: "an update query must be ran using execute" })
+            return
+        }
+
+        db.all(sql)
             .then((result) => {
                 response.status(200)
                 response.json({ result })
